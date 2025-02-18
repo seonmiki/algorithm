f5ee9ea29cb6b7c7b8fc2e409be9362c92a44020
KIM SEONMIN <127717117+seonmiki@users.noreply.github.com>
2025-02-18 20:16:54 +0900

    [Bronze V] Title: We love kriii, Time: 96 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+	}
+}
