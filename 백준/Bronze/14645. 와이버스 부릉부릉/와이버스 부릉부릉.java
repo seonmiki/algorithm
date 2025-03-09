046eccd68d068194e17b407fc8d30ca2ba6daee6
KIM SEONMIN <127717117+seonmiki@users.noreply.github.com>
2025-03-09 22:51:24 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 164 ms, Memory: 17300 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			sc.nextInt();
+			sc.nextInt();
+		}
+		System.out.println("비와이");
+	}
+}
