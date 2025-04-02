f27b408ef87fb502bbafa6085dfefae6aaef0d61
KIM SEONMIN <127717117+seonmiki@users.noreply.github.com>
2025-04-02 20:43:45 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.23 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        int size = A.length;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i = 0; i < size; i++) {
+            answer += A[i] * B[size - 1 - i];
+        }
+
+        return answer;
+    }
+}
