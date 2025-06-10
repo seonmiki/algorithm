b8e4113178ce71a2e49a17f2d3dfced91d7a216d
KIM SEONMIN <127717117+seonmiki@users.noreply.github.com>
2025-06-10 23:34:37 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 12.34 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long solution(int a, int b) {
+        if (a == b) return a;
+        
+        if (a > b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        
+        long answer = 0;
+        for (int i = a; i <= b; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
