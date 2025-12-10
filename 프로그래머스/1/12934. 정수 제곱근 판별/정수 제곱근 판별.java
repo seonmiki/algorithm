079117cd432ba1b66beb9c2bc90b8352aea5fdb0
KIM SEONMIN <127717117+seonmiki@users.noreply.github.com>
2025-12-10 23:53:24 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 20.53 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(long n) {
+        long i = 1;
+        while (i * i <= n) {
+            if (i * i == n) return (i + 1) * (i + 1);
+            i++;
+        }
+        return -1;
+    }
+}
